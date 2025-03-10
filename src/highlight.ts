63ed75179a79b395d1a511900b2a98f68786ebc1
Casper Huang <casper.w.huang@qq.com>
2025-03-10 20:15:29 +0800

    feat: support latex comment for formula block

--- src/highlight.ts
+++ src/highlight.ts
@@ -1,46 +1,61 @@
 import type { ExtensionContext } from 'vscode'
 import { Buffer } from 'node:buffer'
-import { watch } from 'reactive-vscode'
+import { ref, watch } from 'reactive-vscode'
 import { commands, Uri, window, workspace } from 'vscode'
 import { config } from './config'
 
 export function useHighlight(context: ExtensionContext) {
-  watch(() => config.extension.scopes, () => {
-    workspace.fs.readFile(
-      Uri.joinPath(
-        context.extension.extensionUri,
-        'syntaxes',
-        'formula.json',
-      ),
-    ).then((data) => {
-      const o = JSON.parse(data.toString())
-      const os = o._scopes.toSorted()
-      const ns = config.extension.scopes.toSorted()
-
-      if (os.toString() === ns.toString())
-        return
-
-      const n = {
-        ...o,
-        _scopes: ns,
-        injectionSelector: ns.length
-          ? `L:${ns.join(',')} -${
-            o.repository.formula.contentName
-          }`
-          : '',
-      }
-      workspace.fs.writeFile(
-        Uri.joinPath(context.extension.extensionUri, 'syntaxes', 'formula.json'),
-        new Uint8Array(Buffer.from(`${JSON.stringify(n, null, 2)}\n`)),
-      ).then(() => {
-        window.showInformationMessage(
-          'You have updated the scopes should be highlighted, please reload the window to take effect.',
-          'Reload Window',
-        ).then((value) => {
-          if (value === 'Reload Window')
-            commands.executeCommand('workbench.action.reloadWindow')
-        })
-      })
-    })
+  const { readFile, writeFile } = workspace.fs
+  watch(() => config.extension.scopes, async () => {
+    const packagePath = Uri.joinPath(
+      context.extension.extensionUri,
+      'package.json',
+    )
+    const formulaPath = Uri.joinPath(
+      context.extension.extensionUri,
+      'syntaxes',
+      'formula.json',
+    )
+    const packageJSON = await JSON.parse(
+      (await readFile(packagePath)).toString(),
+    )
+    const formulaJSON = await JSON.parse(
+      (await readFile(formulaPath)).toString(),
+    )
+    const configScopes = config.extension.scopes.toSorted()
+    const packageScopes = await packageJSON.contributes.grammars.at(-1).injectTo.toSorted()
+    const formulaScopes = await formulaJSON._scopes.toSorted()
+    const isChnaged = ref(false)
+    if (configScopes.toString() !== packageScopes.toString()) {
+      isChnaged.value = true
+      packageJSON.contributes.grammars[
+        packageJSON.contributes.grammars.length - 1
+      ].injectTo = configScopes
+      await writeFile(
+        packagePath,
+        new Uint8Array(Buffer.from(
+          `${JSON.stringify(packageJSON, null, 2)}\n`,
+        )),
+      )
+    }
+    if (configScopes.toString() !== formulaScopes.toString()) {
+      isChnaged.value = true
+      formulaJSON._scopes = configScopes
+      formulaJSON.injectionSelector = [
+        `L:${configScopes.join(',')}`,
+        '-meta.embedded.litemath.markdown',
+      ].join(' ')
+      await writeFile(
+        formulaPath,
+        new Uint8Array(Buffer.from(
+          `${JSON.stringify(formulaJSON, null, 2)}\n`,
+        )),
+      )
+    }
+    (isChnaged.value && (await window.showInformationMessage(
+      'You have updated the scopes should be highlighted, please reload the window to take effect.',
+      'Reload Window',
+    )) === 'Reload Window')
+    && commands.executeCommand('workbench.action.reloadWindow')
   }, { immediate: true })
 }
