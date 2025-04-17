ea93454fe90db4f9a9cc0ee445244c771133cc60
Casper Huang <casper.w.huang@qq.com>
2025-04-17 23:15:18 +0800

    chore: update

--- src/transformer.ts
+++ src/transformer.ts
@@ -29,9 +29,9 @@ export class FormulaPreview {
 }
 
 class Transformer {
-  private adaptor: ReturnType<typeof liteAdaptor> | undefined = undefined
-  private document: ReturnType<typeof mathjax.document> | undefined = undefined
-  private context: string | undefined = undefined
+  private adaptor: ReturnType<typeof liteAdaptor> | undefined
+  private document: ReturnType<typeof mathjax.document> | undefined
+  private context: string | undefined
   private useAPI
   private mmlPackages = ['action']
 
