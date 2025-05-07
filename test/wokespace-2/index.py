8fe26dc6ffeb08cb86e3f9c97c031e4603b26d4e
Casper Huang <casper.w.huang@qq.com>
2025-05-07 23:52:12 +0800

    test: support multiple folders workspace testing

---
+++ test/wokespace-2/index.py
@@ -0,0 +1,23 @@
+def comment_formula():
+  '''
+  Multiple line formulas:
+  $$
+  \begin{eqnarray*}
+  \sum_{i=1}^{n} i^2 &=& \frac{n(n+1)(2n+1)}{6} \\
+  \sum_{i=1}^{n} i^3 &=& \left(\frac{n(n+1)}{2}\right)^2 \\
+  \sum_{i=1}^{n} i^4 &=& \frac{n(n+1)(2n+1)(3n^2 + 3n - 1)}{30} \\
+  \end{eqnarray*}
+  $$
+  These are some formulas that are used in mathematics.
+  '''
+  pass
+
+# Single line formulas: $$ \mathrm{E = mc^2} $$. This is the Einstein's Equation.
+
+'''
+Preload Commands:
+$$ \textcolor{warning}{\circle} $$  In javascript, `Array.prototype.slice(l, r)` is used to
+   get a copy of a section $$ {\lcro{l, r}} $$ of an array.
+$$ \textcolor{info}{\circle} $$  In css, `border: 1px solid;` is used to create a
+   border around an element looking like $$ \Box{\text{content}} $$.
+'''
