4147606365344f8b2071d0fd4463db054e52d0df
Casper Huang <casper.w.huang@qq.com>
2025-05-07 21:21:02 +0800

    refactor: extract resolve as a utility

--- src/preload.ts
+++ src/preload.ts
@@ -1,41 +1,21 @@
-import { sync as glob } from 'fast-glob'
-import { isAbsolute, join } from 'pathe'
-import {
-  useFsWatcher,
-  watch,
-} from 'reactive-vscode'
+import { useFsWatcher, watch } from 'reactive-vscode'
 import { Uri, workspace } from 'vscode'
 import { config, store } from './config'
-
-function _resolve(path: string): Uri[] {
-  const folders = workspace.workspaceFolders
-  if (isAbsolute(path))
-    return glob(path).map(p => Uri.file(p))
-  if (!folders || !folders.length)
-    return []
-  return glob(join(
-    // TODO: support multiple workspace
-    folders[0].uri.fsPath,
-    path,
-  )).map(p => Uri.file(p))
-}
-
-function resolve() {
-  return Array.from(new Set(
-    config.extension.preload,
-  )).map(p => _resolve(p)).flat()
-}
+import { resolves } from './utils'
 
 async function preload() {
   store.preload.value = await Promise.all(
-    resolve().map(async uri =>
-      (await workspace.fs.readFile(uri)).toString(),
+    resolves(config.extension.preload).map(async p =>
+      (await workspace.fs.readFile(Uri.file(p))).toString(),
     ),
   )
 }
 
 export async function setupWatcher() {
-  const watcher = useFsWatcher(() => resolve().map(uri => uri.fsPath))
+  const watcher = useFsWatcher(() =>
+    resolves(config.extension.preload)
+      .map(p => Uri.file(p).fsPath),
+  )
   watcher.onDidChange(preload)
   watcher.onDidCreate(preload)
   watcher.onDidDelete(preload)
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
+import { sync as glob } from 'fast-glob'
+import { isAbsolute, join } from 'pathe'
+import { workspace } from 'vscode'
+
 export function debounce<T extends (...args: any[]) => any>(
   fn: T,
   delay: number,
@@ -9,4 +13,23 @@ export function debounce<T extends (...args: any[]) => any>(
       fn(...args)
     }, delay)
   }
+}
+
+export function resolve(path: string) {
+  if (isAbsolute(path))
+    return glob(path)
+  const folders = workspace.workspaceFolders
+  if (!folders || !folders.length)
+    return []
+  return glob(join(
+    // TODO: support multiple workspace
+    folders[0].uri.fsPath,
+    path,
+  ))
+}
+
+export function resolves(paths: string[]) {
+  return Array.from(new Set(paths))
+    .map(p => resolve(p))
+    .flat()
 }
