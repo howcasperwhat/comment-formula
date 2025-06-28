d852551c1d193733dc92e6c8fee28ab50a8c2815
Casper Huang <casper.w.huang@qq.com>
2025-06-28 19:19:10 +0800

    feat!: remove expose due to the independence of the responsive system

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,11 @@ import type { ExtensionContext } from 'vscode'
 import { defineExtension } from 'reactive-vscode'
 import { useAnnotation } from './annotation'
 import { useCompletion } from './completion'
-import * as exposed from './store/exposed'
 
 const { activate, deactivate } = defineExtension(
   (context: ExtensionContext) => {
     useAnnotation(context)
     useCompletion(context)
-    return exposed
   },
 )
 export { activate, deactivate }
