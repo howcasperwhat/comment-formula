de38f8afa801dc8bba0aab3775a28906c16f7e77
Casper Huang <casper.w.huang@qq.com>
2025-06-29 21:32:55 +0800

    chore: update lint

--- src/store/constant.ts
+++ src/store/constant.ts
@@ -1,5 +1,6 @@
 export const PLAYGROUND_URL = 'https://howcasperwhat.github.io/mathjax-playground/'
 // @see: https://github.com/microsoft/vscode/blob/main/src/vs/editor/common/config/fontInfo.ts#L14
+// eslint-disable-next-line node/prefer-global/process
 export const GLODEB_LINE_HEIGHT_RATIO = process.platform === 'darwin' ? 1.5 : 1.35
 // use devtools to get the value of 1ex
 export const MATHJAX_TEX_EX = 8.64
